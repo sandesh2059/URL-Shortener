b391813b39adc428c0707d1492788b7be5db1d1e
Sandesh <sandeshchy2059@gmail.com>
2026-01-31 17:45:43 +0545

    redirect added at register view

--- URLshortener/accounts/views.py
+++ URLshortener/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 
 
@@ -6,12 +6,14 @@ def homeView(request):
     return render(request, 'home.html')
 
 def registerView(request):
-    form = CreateUserForm
+    form = CreateUserForm()
 
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('home')
+
     
     context = {'form':form}
     return render(request, 'register.html', context)
