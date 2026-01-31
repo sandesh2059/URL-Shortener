ff85385fade6311bc395315246fbe1980fa34b20
Sandesh <sandeshchy2059@gmail.com>
2026-01-31 17:16:30 +0545

    loginView added

--- URLshortener/accounts/views.py
+++ URLshortener/accounts/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .forms import CreateUserForm
 
 
 def homeView(request):
     return render(request, 'home.html')
+
+def registerView(request):
+    form = CreateUserForm
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+def loginView(request):
+    context = {}
+    return render(request, 'login.html', context)
