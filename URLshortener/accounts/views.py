461f7e841e1b117edb2f56f2bbf4e789c9f4a2d6
Sandesh <sandeshchy2059@gmail.com>
2026-01-31 18:07:25 +0545

    redirect path changed in registerView

--- URLshortener/accounts/views.py
+++ URLshortener/accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def homeView(request):
@@ -12,7 +14,7 @@ def registerView(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('login')
 
     
     context = {'form':form}
@@ -20,5 +22,18 @@ def registerView(request):
 
 
 def loginView(request):
-    context = {}
+    
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'invalid username or password')
+            
+    context={}
     return render(request, 'login.html', context)
