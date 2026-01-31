c08a3fee9db40b4e96a80ccc3d997041079f9469
Sandesh <sandeshchy2059@gmail.com>
2026-01-31 18:25:01 +0545

    templates added in dir

--- URLshortener/URLshortener/settings.py
+++ URLshortener/URLshortener/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'URLshortener.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
