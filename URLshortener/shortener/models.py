54917f6640168077efe50bac5b70e69941f51402
Sandesh <sandeshchy2059@gmail.com>
2026-01-31 19:36:02 +0545

    model created for UrlShortener

--- URLshortener/shortener/models.py
+++ URLshortener/shortener/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UrlShortener(models.Model):
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.original_url
+
+
