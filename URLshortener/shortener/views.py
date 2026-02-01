dae77dca3b8bebe7182b1e12a6bc8e35c1d75ff9
Sandesh <sandeshchy2059@gmail.com>
2026-02-01 13:14:25 +0545

    views changed

--- URLshortener/shortener/views.py
+++ URLshortener/shortener/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import UrlShortener
+from .serializers import LongUrlSerializer
+from .permissions import CanCreateShortURL
 
-# Create your views here.
+
+class LongUrlApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated ,CanCreateShortURL]
+
+    
+    def get(self, request):
+        urls = UrlShortener.objects.all()
+        serializer = LongUrlSerializer(urls, many = True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = LongUrlSerializer(data=request.data, context = {'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    
+def longurl_list(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    urls = UrlShortener.objects.all()
+    return render(request, 'longurl.html', context={'urls':urls})
+
